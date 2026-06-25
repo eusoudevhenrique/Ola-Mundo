e584c15253ab6842b910fc2ed90601b59b79c7d4
eusoudevhenrique <henriqueribeiro4842@gmail.com>
2026-06-24 23:15:50 -0300

    fiz mudanças no codigo
    .

--- site-exemplo/index.py
+++ site-exemplo/index.py
@@ -1,1 +1,11 @@
-print("Calculadora de IMC")
+print("=== Calculadora de IMC ===")
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite seu peso: "))
+altura = float(input("Digite sua altura: "))
+
+print("Nome: " + nome)
+print("Idade: " + str(idade))
+print("Peso: " + str(peso) + " kg")
+print("Altura: " + str(altura) + " m")
